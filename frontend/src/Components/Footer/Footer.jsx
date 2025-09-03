ec14ca408a8d7bc5903a268d16615e92b5eaee77
Geromarquezz <geritomarque@gmail.com>
2025-09-02 21:42:08 -0300

    Correccion de frontend

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import './Footer.css'
+import footer_logo from '../Assets/logo_big.png'
+import instagram_icon from '../Assets/instagram_icon.png'
+import pintester_icon from '../Assets/pintester_icon.png'
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
+
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-logo">
+            <img src={footer_logo} alt="" />
+            <p>Shooper</p>
+        </div>
+        <ul className="footer-links">
+            <li className="">Company</li>
+            <li className="">Products</li>
+            <li className="">Offices</li>
+            <li className="">About Us</li>
+            <li className="">Contact Us</li>
+        </ul>
+        <div className="footer-social-icon">
+            <div className="footer-icons-container">
+                <img src={instagram_icon} alt="" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={pintester_icon} alt="" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={whatsapp_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <p>© 2025 Shooper. All rights reserved.</p>
+        </div>
+    </div>
+  )
+}
---
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png';
+import star_dull_icon from '../Assets/star_dull_icon.png';
+
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+
+        </div>
+        <div className="productdisplay-img">
+            <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="displayright-star">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(122)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">
+                    ${product.old_price}
+                </div>
+                <div className="productdisplay-right-price-new">
+                    ${product.new_price}
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default ProductDisplay
